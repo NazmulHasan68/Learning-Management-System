c36812db9b84f31175f0c400a626226e176eaec6
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-30 13:08:07 +0600

    Create user Controller

--- server/index.js
+++ server/index.js
@@ -2,10 +2,25 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './Database/dbConnect.js'
+import userRoute from './routes/user.Route.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 
 const app = express()
-const PORT = 8080 
+const PORT = 8000 
+
+//apis
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:'http:localhost:8000',
+    credentials:true
+}))
+app.use("/api/v1/user", userRoute)
+
+app.get('/home', (req, res) => {
+    res.status(200).json({ success: true, message: "Hello, I am coming from the backend" });
+  });
 
 
 app.listen(PORT , (req, res)=>{
