13770de4eb938eb91c5c592d55fe551fd0722c09
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-02 10:10:18 +0600

    get course by Id

--- server/controllers/course.Controller.js
+++ server/controllers/course.Controller.js
@@ -53,7 +53,7 @@ export const editCourse = async (req, res) => {
     const courseId = req.params.courseId;
     const {
       courseTitle,
-      subTitlw,
+      subTitle,
       description,
       category,
       courseLevelm,
@@ -79,7 +79,7 @@ export const editCourse = async (req, res) => {
 
     const updateData = {
       courseTitle,
-      subTitlw,
+      subTitle,
       description,
       category,
       courseLevelm,
@@ -100,3 +100,25 @@ export const editCourse = async (req, res) => {
     });
   }
 };
+
+
+export const getCourseById = async(req, res)=>{
+    try {
+        const {courseId} = req.params;
+        const course = await Course.findById(courseId)
+        if(!course){
+            return res.status(404).json({
+                message: "Course not found!"
+            })
+        }
+        return res.status(200).json({
+            course
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+        message: "Failed to course by Id!",
+        });
+    }
+}
--- server/routes/course.Route.js
+++ server/routes/course.Route.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated.js'
 import upload from '../utils/multer.js'
-import { createCourse, editCourse, getCreatorCourses } from '../controllers/course.Controller.js'
+import { createCourse, editCourse, getCourseById, getCreatorCourses } from '../controllers/course.Controller.js'
 
 const router = express.Router()
 router.route('/').post(isAuthenticated, createCourse)
 router.route('/').get(isAuthenticated, getCreatorCourses)
 router.route('/:courseId').put(isAuthenticated,upload.single('courseThumbnail'), editCourse)
+router.route('/:courseId').get(isAuthenticated, getCourseById)
 
 
 export default router
