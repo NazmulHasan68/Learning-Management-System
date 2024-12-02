fe4d86d5b3de1422af78ce16030d2c73358fac04
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-02 09:29:38 +0600

    Course controller

--- server/controllers/course.Controller.js
+++ server/controllers/course.Controller.js
@@ -1,25 +1,102 @@
-import { Course } from "../models/course.model.js"
+import { Course } from "../models/course.model.js";
+import { deleteVideoFromCloudinary, uploadMedia } from "../utils/cloudinary.js";
 
-export const createCourse = async(req, res)=>{
-    try {
-        const {courseTitle , category} = req.body
-        if(!courseTitle || !category){
-            return res.status(400).json({
-                message: "Course title and category is required!"
-            })
-        }
-        const course = await Course.create({
-            courseTitle,
-            category,
-            creator : req.id
-        })
+export const createCourse = async (req, res) => {
+  try {
+    const { courseTitle, category } = req.body;
+    console.log(category, courseTitle);
 
-        return res.status(201).json({
-            success:true,
-            course, 
-            message: "Course created!"
-        })
-    } catch (error) {
-        return req.status(500).json("Fail to create course")
+    if (!courseTitle || !category) {
+      return res.status(400).json({
+        message: "Course title and category is required!",
+      });
     }
-}
+    const course = await Course.create({
+      courseTitle,
+      category,
+      creator: req.id,
+    });
+
+    return res.status(201).json({
+      success: true,
+      course,
+      message: "Course created!",
+    });
+  } catch (error) {
+    return req.status(500).json("Fail to create course");
+  }
+};
+
+export const getCreatorCourses = async (req, res) => {
+  try {
+    const userId = req.id;
+    const courses = await Course.find({ creator: userId });
+    if (!courses)
+      return res.status(404).json({
+        course: [],
+        message: "Course not found!",
+      });
+    return res.status(200).json({
+      courses,
+    });
+  } catch (error) {
+    console.log(error);
+    return req.status(500).json("Fail to create course");
+  }
+};
+
+
+
+
+export const editCourse = async (req, res) => {
+  try {
+    const courseId = req.params.courseId;
+    const {
+      courseTitle,
+      subTitlw,
+      description,
+      category,
+      courseLevelm,
+      coursePrice,
+    } = req.body;
+    const thumbnail = req.file;
+
+    let course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({
+        message: "Course not found!",
+      });
+    }
+    let courseThumbnail;
+    if (thumbnail) {
+      if (course.courseThumbnail) {
+        const publicId = course.courseThumbnail.split("/").pop().split(".")[0];
+        await deleteVideoFromCloudinary(publicId); // delete old image
+      }
+      //upload a thumbnail on cloudinary
+      courseThumbnail = await uploadMedia(thumbnail.path)
+    }
+
+    const updateData = {
+      courseTitle,
+      subTitlw,
+      description,
+      category,
+      courseLevelm,
+      coursePrice,
+      courseThumbnail:courseThumbnail?.secure_url
+    };
+
+    course = await Course.findByIdAndUpdate(courseId, updateData, {new:true})
+
+    return res.status(200).json({
+        course,
+        message: "Course Updated successfully!"
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to update course!",
+    });
+  }
+};
--- server/routes/course.Route.js
+++ server/routes/course.Route.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { isAuthenticated } from '../middlewares/isAuthenticated.js'
 import upload from '../utils/multer.js'
-import { createCourse } from '../controllers/course.Controller.js'
+import { createCourse, editCourse, getCreatorCourses } from '../controllers/course.Controller.js'
 
 const router = express.Router()
 router.route('/').post(isAuthenticated, createCourse)
+router.route('/').get(isAuthenticated, getCreatorCourses)
+router.route('/:courseId').put(isAuthenticated,upload.single('courseThumbnail'), editCourse)
 
 
 export default router
