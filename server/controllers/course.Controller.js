93d885a42b41485922a97fae068e6bf6126bf9c3
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-03 10:02:27 +0600

    Publich functionality

--- server/controllers/course.Controller.js
+++ server/controllers/course.Controller.js
@@ -291,3 +291,37 @@ export const getLectureById = async(req, res)=>{
     })
   }
 }
+
+
+
+//publish unpnlish xourse logic
+export const togglePublishCourse = async(req, res)=>{
+  try {
+    const {courseId} = req.params
+    const {publish} = req.query; // true or false
+
+    const course = await Course.findById(courseId)
+
+    if(!course){
+      return res.status(404).json({
+        message:"Course not found!"
+      })
+    }
+
+    //publish status based on the query parameter
+    course.isPublished = publish === 'true'
+  
+    await course.save()
+
+    const statusMessage = course.isPublished ? "Publish" : "Unpublished";
+    return res.status(200).json({
+      message : `Course is ${statusMessage}`
+    })
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message : "Failed to publish and upblish"
+    })
+  }
+}
