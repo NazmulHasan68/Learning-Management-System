f541b60350730abde35327d19aec7f91762e5fd5
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-07 23:45:49 +0600

    purchaseApi conect to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./routeReducer.js";
 import { authApi } from "@/features/api/authApi.js";
 import { courseApi } from "@/features/api/courseApi.js";
+import { purchaseApi } from "@/features/api/purchaseApi.js"
 
 export const appStore = configureStore({
   reducer: {
     auth: rootReducer, 
     [authApi.reducerPath]: authApi.reducer, // authApi state
     [courseApi.reducerPath]: courseApi.reducer, // Add courseApi state
+    [purchaseApi.reducerPath] : purchaseApi.reducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authApi.middleware, courseApi.middleware), // Include middlewares for both APIs
