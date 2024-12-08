5cb68f5f2a80ab12182490957a614c2f59878b66
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-08 19:16:47 +0600

    Add course

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 // CORS Configuration
 app.use(cors({
     origin: 'http://localhost:5173',  // Allow requests from frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Include cookies if needed
 }));
--- server/models/course.model.js
+++ server/models/course.model.js
@@ -17,7 +17,7 @@ const courseSchema = new mongoose.Schema({
     },
     courseLevel : {
         type : String,
-        enum : ["Beginner", "Medium", "Advance"]
+        enum : ["beginner", "intermediate", "advanced"]
     },
     coursePrice : {
         type : Number,
